89fd73d6f6d1bf194fb24f523bcb3cfc70d7525e
ingrid <ingrid7613@gmail.com>
2022-10-06 16:47:23 +0800

    增加font-awesome-icon

--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ import axios from "axios"
 
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+// vuex目前沒使用
+// import store from './store'
+
+/* import the fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faSearch, faClone, faList, faFilter, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faUserSecret, faSearch, faClone, faList, faFilter, faMagnifyingGlass)
 
 
 const app = createApp(App)
@@ -18,9 +27,10 @@ const app = createApp(App)
 app.use(ElementPlus)
 // Vue實體使用函式庫 -end
 
-app.use(store)
 app.use(router)
+// app.use(store)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.config.globalProperties.axios=axios
-
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     {{title}}
+    <font-awesome-icon icon="fa-solid fa-user-secret" />
+    <font-awesome-icon icon="fa-solid fa-search" />
+    <font-awesome-icon icon="fa-solid fa-list" />
+    <font-awesome-icon icon="fa-solid fa-clone" />
+    <font-awesome-icon icon="fa-solid fa-filter" />
+    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
     <router-link :to="{name: 'frontpage'}">前台</router-link> |
     <router-link :to="{name: 'backpage'}">後台</router-link> |
   </div>
