775768158a77dcf9230e71ebabdb3b3de1b29350
ingrid <ingrid7613@gmail.com>
2022-10-11 00:53:09 +0800

    更改deploy sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ set -e
 npm run build
 
 # 移動至到打包後的靜態產品目錄 
-cd resumetest
+cd dist
 
+# 複製dist/index.html並改名為404.html
 # 讓github page重新整理正常(沒有複製則會到github的預設404 page)
 cp index.html 404.html
 
@@ -14,14 +15,14 @@ cp index.html 404.html
 # 因此在進入資料夾後先初始化git
 git init 
 git add -A
-git commit -m 'Deploy 20220919'
-
+git commit -m 'Deploy 20221010'
 
 # 申請GitHub Personal access tokens，記得不要將這個檔案推到git，token會暴露
 # 將 dist資料夾中的內容推送至遠端github-pages分支中
 # 並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-git push -f https://<GitHub Personal access tokens>@github.com/ingridkao/resumetest.git master:github-pages
+# git push -f https://<GitHub Personal access tokens>@github.com/ingridkao/resumetest.git master:github-pages
+
+git checkout -b github-pages
+git remote add origin https://github.com/ingridkao/resumetest.git
+git push -f https://<GitHub Personal access tokens>@github.com/ingridkao/resumetest.git
 cd -
-
-
-# 在.gitignore中增加 /resumetest
